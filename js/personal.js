726070e7a130ba203aae53ea825eed127794cc89
InesFrederickx <ines.frederickx@student.ehb.be>
2023-01-26 11:54:34 +0100

    changed padding on box, made a an array checked

--- js/personal.js
+++ js/personal.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var itemForm = document.getElementById('box'); // getting the parent container of all the checkbox inputs
-var checkBoxes = itemForm.querySelectorAll('input[type="checkbox"]'); // get all the check box
-document.getElementById('submit').addEventListener('click', getData); //add a click event to the save button
+var checkBoxes = itemForm.querySelectorAll("#checkBox"); // get all the check box
+document.getElementById('next').addEventListener('click', getData); //add a click event to the save button
 
 let result = [];
 
 function getData() { // this function will get called when the save button is clicked
+
     result = [];
     checkBoxes.forEach(item => { // loop all the checkbox item
         if (item.checked) { //if the check box is checked
@@ -17,8 +18,12 @@ function getData() { // this function will get called when the save button is cl
             result.push(data); //stored the objects to result array
         }
     })
-    console.log(data)
-    //document.querySelector('.result').textContent = JSON.stringify(result); // display result
+    drawPath();
 }
 
-// var chackbox = document.getElementsByClassName('checkBox')
+function drawPath() {
+    console.log(result);
+
+
+
+}

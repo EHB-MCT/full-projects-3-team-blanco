fc29bfe79c9d65687ebaaa48ffeb7e36cede04d1
Judith Bosmans <judobosmans@gmail.com>
2023-01-31 11:46:16 +0100

    WIP deploy

--- js/test1.js
+++ js/test1.js
@@ -1,71 +1,35 @@
-function off1() {
-    document.getElementById("overlay").style.display = "none";
-}
+var arrLang = {
+    'en': {
+        'Settings': 'Settings',
+        'Language': 'Language:',
+        'Select language:': 'Select language:',
+        'Elevator': 'Elevator',
+        'Don t want to follow a specific route but you just want to find the way to a certain room? This is possible at the ': 'Don t want to follow a specific route but you just want to find the way to a certain room? This is possible at the ',
+        'Destinations': 'Destinations',
+        'Would you like a general view of the museum then you can find this at': 'Would you like a general view of the museum then you can find this at',
+        'Map': 'Map',
+        'an interactive display of the museum': 'an interactive display of the museum',
+    },
+    'nl': {
+        'Settings': 'Instellingen',
+        'Language': 'Taal:',
+        'Select language:': 'Selecteer taal',
+        'Elevator': 'Lift',
+        'Don t want to follow a specific route but you just want to find the way to a certain room? This is possible at the ': 'Wil je geen specifieke route volgen maar wel snel de weg vinden naar een bepaalde plaats in het museum? Dat kan via',
+        'Destinations': 'Bestemmingen',
+        'Would you like a general view of the museum then you can find at': 'Wil je graag een algemeen overzicht van het museum dan kan je bij',
+        'Map': 'Kaart',
+        'an interactive display of the museum.': 'een 3D weergave vinden via onze interactieve kaart.',
+    }
+};
 
-function on1() {
-    document.getElementById("overlay").style.display = "block";
-}
+// Process translation
+$(function () {
+    $('.button1').click(function () {
+        var lang = $(this).attr('id');
 
-function off2() {
-    document.getElementById("overlay2").style.display = "none";
-}
-
-function on2() {
-    document.getElementById("overlay2").style.display = "block";
-}
-
-function off3() {
-    document.getElementById("overlay3").style.display = "none";
-}
-
-function on3() {
-    document.getElementById("overlay3").style.display = "block";
-}
-
-function off4() {
-    document.getElementById("overlay3").style.display = "none";
-}
-
-function on4() {
-    document.getElementById("overlay3").style.display = "block";
-}
-
-function off5() {
-    document.getElementById("overlay3").style.display = "none";
-}
-
-function on5() {
-    document.getElementById("overlay3").style.display = "block";
-}
-
-function off6() {
-    document.getElementById("overlay3").style.display = "none";
-}
-
-function on6() {
-    document.getElementById("overlay3").style.display = "block";
-}
-
-function off7() {
-    document.getElementById("overlay3").style.display = "none";
-}
-
-function on7() {
-    document.getElementById("overlay3").style.display = "block";
-}
-
-function off8() {
-    document.getElementById("overlay3").style.display = "none";
-}
-
-function on8() {
-    document.getElementById("overlay3").style.display = "block";
-}
-
-function off9() {
-    document.getElementById("overlay3").style.display = "none";
-}
-
-function on9() {
-    document.getElementById("overlay3").style.display = "block";
-}
+        $('#lang').each(function (index, item) {
+            $(this).text(arrLang[lang][$(this).attr('key')]);
+        });
+    });
+});

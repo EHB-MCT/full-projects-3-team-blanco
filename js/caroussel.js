1ef6f2e3d146ae0cb72fbcc802adc59fd44aae87
Judith Bosmans <judobosmans@gmail.com>
2023-02-01 11:05:38 +0100

    WIP push for acces images on github

---
+++ js/caroussel.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//     var currentImage = 0;
+//     var images = $('.carousel img');
+//     var numImages = images.length;
+
+//     function nextImage() {
+//         images.eq(currentImage).css('opacity', 0);
+//         currentImage = (currentImage + 1) % numImages;
+//         images.eq(currentImage).css('opacity', 1);
+//     }
+
+//     setInterval(nextImage, 1000);
+// });

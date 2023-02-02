50d2d6ea8c9c3733f78b58a72fdbcde76cbe73b6
InesFrederickx <ines.frederickx@student.ehb.be>
2023-02-02 23:01:29 +0100

    dom manipulatin WIP, css for title

--- js/personal.js
+++ js/personal.js
@@ -1,28 +1,22 @@
 'use strict';
 
-var itemForm = document.getElementById('box'); // getting the parent container of all the checkbox inputs
-var checkBoxes = itemForm.querySelectorAll('input[type="checkBox"]'); // get all the check box
-document.getElementById('next').addEventListener('click', getData); //add a click event to the save button
+var itemForm = document.getElementById('box');
+var checkBoxes = itemForm.querySelectorAll('input[type="checkBox"]');
+document.getElementById('next').addEventListener('click', getData);
 
-let result = [];
+export let result = [];
 
-function getData() { // this function will get called when the save button is clicked
+function getData() {
 
     result = [];
-    checkBoxes.forEach(item => { // loop all the checkbox item
-        if (item.checked) { //if the check box is checked
-            let data = { // create an object
+    checkBoxes.forEach(item => {
+        if (item.checked) {
+            let data = {
                 item: item.value,
                 selected: item.checked
             }
-            result.push(data); //stored the objects to result array
+            result.push(data);
         }
     })
     drawPath()
-}
-
-function drawPath() {
-
-    console.log(result);
-
 }
--- js/personalmap.js
+++ js/personalmap.js
@@ -1,22 +1,31 @@
 "use strict";
 
-document.getElementById('4_lift').addEventListener('click', FourLiftToThree);
-document.getElementById('4_trapboven').addEventListener('click', FourUpperToThree);
-document.getElementById('4_trapbeneden').addEventListener('click', FourDownToThree);
-document.getElementById('4_lift').addEventListener('click', nextRoute);
-document.getElementById('4_lift').addEventListener('click', nextRoute);
-document.getElementById('4_lift').addEventListener('click', nextRoute);
-document.getElementById('4_lift').addEventListener('click', nextRoute);
+
+import {
+    result
+} from "./personal.js";
 
 
-function FourLiftToThree() {
-    document.getElementById("img").src = "../images/museum_routes/living_evolution_3.png";
+document.getElementById('1_boven').addEventListener('click', OneLiftToThree);
+
+function OneLiftToThree() {
+    document.getElementById("img").src = "../images/museum_routes/living_3.png";
+
+    const terms = document.createElement('area');
+    terms.setAttribute('id', '3_boven');
+    terms.setAttribute('target', '_blank');
+    terms.setAttribute('coords', '314,210,23');
+    terms.setAttribute('shape', 'circle');
+
+    document.getElementById('3_boven').addEventListener('click', ThreeLiftToFour);
 };
 
-function FourUpperToThree() {
-    document.getElementById("img").src = "../images/museum_routes/living_evolution_3.png";
-};
+function ThreeLiftToFour() {
+    document.getElementById("img").src = "../images/museum_routes/living_evolution_4_elevator.png";
 
-function FourDownToThree() {
-    document.getElementById("img").src = "../images/museum_routes/living_evolution_3.png";
+    const terms = document.createElement('area');
+    terms.setAttribute('id', '4_boven');
+    terms.setAttribute('target', '_blank');
+    terms.setAttribute('coords', '314,211,22');
+    terms.setAttribute('shape', 'circle');
 };

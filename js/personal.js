694afbb9e2b3e4a3a9e64686402a007ef78c53d0
InesFrederickx <ines.frederickx@student.ehb.be>
2023-02-03 00:18:10 +0100

    added css to personalmap, added commentary

--- js/personal.js
+++ js/personal.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// de array "result" zouden we exporteren naar personalmap.js om de meest accurate route te krijgen
+
 var itemForm = document.getElementById('box');
 var checkBoxes = itemForm.querySelectorAll('input[type="checkBox"]');
 document.getElementById('next').addEventListener('click', getData);
--- js/personalmap.js
+++ js/personalmap.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 /*import {
     result
 } from "./personal.js";*/
